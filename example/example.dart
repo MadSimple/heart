c3bb39e85d4a93e30fb5c6c2ad8be70713c089a1
MadSimple <92187165+MadSimple@users.noreply.github.com>
2024-06-01 21:09:44 -0500

    Add files via upload

--- example/example.dart
+++ example/example.dart
@@ -2,22 +2,8 @@ import 'package:heart/heart.dart';
 
 /// Small sample of package features
 void main() {
-  List<String> cardValues = [
-        'A',
-        '2',
-        '3',
-        '4',
-        '5',
-        '6',
-        '7',
-        '8',
-        '9',
-        '10',
-        'J',
-        'Q',
-        'K',
-      ] *
-      4;
+  List<String> cardValues =
+      ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'] * 4;
 
   List<String> cardSuits =
       ['spades'] * 13 + ['hearts'] * 13 + ['diamonds'] * 13 + ['clubs'] * 13;
